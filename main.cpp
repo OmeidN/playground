f4c342935666427fa56a1b0f0cb7edea977979de
Omeid Nadery <omeid.n@icloud.com>
2024-05-16 20:16:21 -0700

    Reversing a string no loops

--- main.cpp
+++ main.cpp
@@ -6,7 +6,33 @@
 
 using namespace std;
 
+// Partners : Omeid Nadery & Kyle Driver
+
+string reverseName(string& name){
+	string temp = name;
+	string reversed = "";
+	char store;
+	if(temp.size() != 0){
+		char store = temp[0];
+		temp = temp.substr(1, temp.size()-1);
+		reversed += (reverseName(temp));
+		reversed.push_back(store);		
+	}
+	return reversed;
+	
+}
+
+
 int main()
 {
+	string name = "Omeid Nadery";
+	cout << name << endl;
+
+	name = reverseName(name);
+
+	cout << "Reversed: " << name << endl;
 	return EXIT_SUCCESS;
 }
+
+
+
